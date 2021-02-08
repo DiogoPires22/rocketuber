b0a39eb46526ddea02de2ae4409d4f78492eeda9
DiogoPires22 <diogo.pcaires@gmail.com>
2021-02-08 16:36:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Container, Map} from './components/styles';
+
+const App: React.FC = () => (
+  <Container>
+    <Map />
+  </Container>
+);
+
+export default App;
---
+++ src/components/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+import MapBox from './mapbox';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Map = styled(MapBox)`
+  flex: 1;
+`;
